c937fb3bb0c7d121eea73056897a9eea94dc50d1
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-07-05 10:59:15 +0300

    Added ResponseDto

---
+++ SharedLibrary/Dtos/ResponseDto.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SharedLibrary.Dtos
+{
+    public class ResponseDto<T> where T:class
+    {
+        public T Data { get; private set; }
+        public int StatusCode { get; private set; }
+
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+        public ErrorDto Error { get; private set; }
+
+        public static ResponseDto<T> Success(T data, int statusCode)
+        {
+            return new ResponseDto<T> 
+            {
+                Data = data,
+                StatusCode = statusCode,
+                IsSuccessful = true 
+            };
+        }
+        public static ResponseDto<T> Success(int statusCode)
+        {
+            return new ResponseDto<T>
+            {
+                Data = default,
+                StatusCode = statusCode,
+                IsSuccessful = true 
+            };
+        }
+        public static ResponseDto<T> Fail(ErrorDto errorDto, int statusCode)
+        {
+            return new ResponseDto<T> 
+            { 
+                Error = errorDto, 
+                StatusCode = statusCode ,
+                IsSuccessful = false
+            };
+        }
+        public static ResponseDto<T> Fail(string errorMessage,int statusCode, bool isShow)
+        {
+            ErrorDto errorDto = new ErrorDto(errorMessage, isShow);
+            return new ResponseDto<T> 
+            { 
+                Error = errorDto,
+                StatusCode = statusCode ,
+                IsSuccessful = false
+            };
+        }
+
+
+    }
+}
