0af8cfdcd7f0618ebb0ab0b1cd2cc936b2d5967e
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-07-06 15:28:17 +0300

    implemented generic repository

--- AuthServer.Core/Repositories/IProductRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using AuthServer.Core.Models;
-
-namespace AuthServer.Core.Repositories
-{
-    public interface IProductRepository : IGenericRepository<Product>
-    {
-        public void SpeacialMethodForProduct();
-    }
-}
