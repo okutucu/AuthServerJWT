8d77bc885329da9d87f9c4313f820404336f0dd3
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-07-06 10:17:37 +0300

    Added services

--- AuthServer.Core/Services/IService.cs
+++ AuthServer.Core/Services/IService.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using SharedLibrary.Dtos;
 
 namespace AuthServer.Core.Service
 {
-    public interface IService<TEntity> where TEntity : class
+    public interface IService<TEntity,TDto> where TEntity : class where TDto: class
     {
-        Task<TEntity> GetByIdAsync(int id);
-        Task<IEnumerable<TEntity>> GetAllAsyn();
-        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
-        Task AddAsync(TEntity entity);
-        void Remove(TEntity entity);
-        TEntity Update(TEntity entity);
-
+        Task<ResponseDto<TDto>> GetByIdAsync(int id);
+        Task<ResponseDto<IEnumerable<TDto>>> GetAllAsyn();
+        Task<ResponseDto<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
+        Task<ResponseDto<TDto>> AddAsync(TEntity entity);
+        Task<ResponseDto<NoDataDto>> Remove(TEntity entity);
+        Task<ResponseDto<NoDataDto>> Update(TEntity entity);
 
     }
 }
