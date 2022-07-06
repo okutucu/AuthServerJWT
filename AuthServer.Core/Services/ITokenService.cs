43a2b7f42a9d8110d6bce259733b93355333d801
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-07-06 11:17:31 +0300

    Added TokenService

---
+++ AuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using AuthServer.Core.Configurations;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+
+namespace AuthServer.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+        ClientTokenDto CreateToken(Client client);
+    }
+}
