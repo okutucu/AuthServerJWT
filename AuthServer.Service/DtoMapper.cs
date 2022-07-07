716d82d59ebe6d5c1ec4960431b87c0d2c047bb3
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-07-07 15:23:04 +0300

    Added Automapper anb Object mapper

---
+++ AuthServer.Service/DtoMapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AutoMapper;
+
+namespace AuthServer.Service
+{
+     class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
---
+++ AuthServer.Service/ObjectMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace AuthServer.Service
+{
+    public static class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() => 
+        {
+            MapperConfiguration config = new MapperConfiguration(cfg =>
+           {
+               cfg.AddProfile<DtoMapper>();
+           });
+
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => lazy.Value;
+    }
+}
