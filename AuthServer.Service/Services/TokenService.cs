f552c0f0a42dce9526cf7a8f0b8f68df8d94227a
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-07-13 13:57:21 +0300

    Create Refresh Token and added RandomNumberGenerator

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography;
+using AuthServer.Core.Configurations;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Configurations;
+
+namespace AuthServer.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly CustomTokenOption _tokenOption;
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOption> options)
+        {
+            _userManager = userManager;
+            _tokenOption = options.Value;
+        }
+
+        private string CreateRefreshToken()
+        {
+            byte[] numberByte = new Byte[32];
+
+            using RandomNumberGenerator rnd = RandomNumberGenerator.Create();
+
+            rnd.GetBytes(numberByte);
+
+            return Convert.ToBase64String(numberByte);
+        }
+
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateToken(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
